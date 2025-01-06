9d662565e00de8efe0232333eea0a92935f5a3bd
d16ntless <134496197+d16ntless@users.noreply.github.com>
2025-01-06 18:39:48 +0800

    val.py

--- val.py
+++ val.py
@@ -3,14 +3,14 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('runs/train/fire/rmb/wiou/WIoUv3 a=1.9 d=3/weights/best.pt')
-    model.val(data='/study/datasets/fire/fire.yaml',
-              split='val',
+    model = YOLO('weights/best.pt')
+    model.val(data='',
+              split='',
               imgsz=640,
               batch=16,
               # iou=0.7,
               # rect=False,
               # save_json=True, # if you need to cal coco metrice
-              project='runs/val/fire',
-              name='yolov8s-C2f-iRMB-Cascaded-BiLevelRoutingAttention-WIoUv3 a=1.9 d=3',
+              project='',
+              name='',
               )
